e1bda8c8f1740fd6e69f4916880b92a43f6f5f57
Doğuş Erkan <doguscanerkan@gmail.com>
2023-04-17 15:19:30 +0300

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-import { ExampleComponent } from 'button-dgs'
+import { Button, ExampleComponent } from 'button-dgs'
 import 'button-dgs/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+    <Button text='Primary Button' type='primary'/>
+    <Button text='Default Button'/>
+    <Button text='Dashed Button' type='dashed'/>
+    <Button text='Text Button' type='text'/>
+    <Button text='Linked Button' type='linked'/>
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+
+export const Button = ({type,...props}) => {
+  return (<button className={styles[type]} {...props}>{props.text}</button>)
 }
